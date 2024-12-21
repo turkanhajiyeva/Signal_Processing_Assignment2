723412a6d9d4df65161995500656dbc10402fb6b
turkanhajiyeva <thajiyeva15574@ada.edu.az>
2024-12-21 18:27:48 +0400

    task 2 and 3

--- question2and3.py
+++ question2and3.py
@@ -8,10 +8,22 @@ x1 = 5 * np.cos(0.3 * t) + np.sin(0.1 * t)
 
 x2 = 3 * np.cos(0.2 * t) - 2 * np.sin(0.4 * t)
 
-plt.plot(t, x1, label = 'x1')
+x1m = x1 * np.cos(10 * t)
 
-plt.plot(t, x2, label = 'x2')
+x2m = x2 * np.cos(20 * t)
 
+plt.plot(t, x1m, label = 'x1m')
+plt.legend()
+plt.figure()
+
+plt.plot(t, x2m, label = 'x2m')
+plt.legend()
+
+x = x1m + x2m
+
+plt.figure()
+
+plt.plot(t, x, label = 'x1m + x2m')
 plt.legend()
 
 plt.show()
